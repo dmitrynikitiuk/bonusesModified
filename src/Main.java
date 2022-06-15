4f7b05eed0b2dc2f95e15d463d551028f4154a27
dmitrynikitiuk <dmitrynikitiuk@gmail.com>
2022-06-15 21:18:06 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-int account = 100;
-int fill = 8900;
-int bonus;
-if (fill > 1000) {bonus = fill / 100;} else {bonus = 0;};
-int sum = account + fill + bonus;
+        int account = 100;
+        int fill = 8900;
+        int bonus;
+        if (fill > 1000) {
+            bonus = fill / 100;
+        } else {
+            bonus = 0;
+        }
+        ;
+        int sum = account + fill + bonus;
         System.out.println("Итоговая сумма на счёте составляет:" + sum);
         System.out.println("Сумма бонусных рублей составляет:" + bonus);
-}
+    }
 }
